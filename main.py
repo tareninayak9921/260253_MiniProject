44966f69b9c523ce067829e0ad3ed0a8a602d5a8
tareninayak9921 <tareninayak9921@gmail.com>
2021-04-28 18:37:36 +0530

    Display Record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-z=1
-while(True):
+z = 1
+while True:
     print("\n 1. Add Record")
     print("\n 2. Display All Record")
     print("\n 3. Search Student Record By Name")
@@ -9,10 +9,10 @@ while(True):
     print("\n 6. Update Student Record")
     print("\n 7. Exit")
 
-    n=int(print(input("Enter Your Choice:  ")))
-    if(n==7):
+    n = int(print(input("Enter Your Choice:  ")))
+    if n == 7:
         break
-    elif(n==1):
+    elif n == 1:
         print("Enter The Student Details\n")
         name = input("Enter Name: ")
         roll = input("Enter Roll Number: ")
@@ -22,16 +22,18 @@ while(True):
         file = open("Record/student.txt","a")
         file.write(name+"-"+roll+"-"+cls+"-"+fee+"-"+percentage+"\n")
         file.close()
-    elif(n==2):
+    elif n == 2:
         print("\n\n List Of Present Records\n\n")
         print("NAME---ROLLNO---CLASS---FEES---PERCENTAGE")
-        file=open("Record/student.txt","r")
-        flag=0
+        file = open("Record/student.txt", "r")
         while True:
             content = file.readline()
             length = len(content)
-            if length == 0 :
+            if length == 0:
                 break
-            print()
+            print(content.strip())
+        file.close()
+
+
 
 
