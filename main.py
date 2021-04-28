2a48a6f9ea2ad869a9e0a3eccde14bb12fae17f2
tareninayak9921 <tareninayak9921@gmail.com>
2021-04-28 18:34:42 +0530

    Display Record

--- main.py
+++ main.py
@@ -16,5 +16,22 @@ while(True):
         print("Enter The Student Details\n")
         name = input("Enter Name: ")
         roll = input("Enter Roll Number: ")
+        cls = input("Enter Class: ")
+        fee = input("Enter Fees: ")
+        percentage = input("Enter The Percentage: ")
+        file = open("Record/student.txt","a")
+        file.write(name+"-"+roll+"-"+cls+"-"+fee+"-"+percentage+"\n")
+        file.close()
+    elif(n==2):
+        print("\n\n List Of Present Records\n\n")
+        print("NAME---ROLLNO---CLASS---FEES---PERCENTAGE")
+        file=open("Record/student.txt","r")
+        flag=0
+        while True:
+            content = file.readline()
+            length = len(content)
+            if length == 0 :
+                break
+            print()
 
 
