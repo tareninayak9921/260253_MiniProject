88f641cba3472e6cd7d153fd0ad798ea93a4812e
tareninayak9921 <tareninayak9921@gmail.com>
2021-04-28 19:22:46 +0530

    Running Till 5rd Function

--- main.py
+++ main.py
@@ -50,12 +50,55 @@ while True:
                 print("Class: ", arr[2])
                 print("Fees: ", arr[3])
                 print("Percentage: ", arr[4])
-                flag=1
+                flag = 1
                 break
             if flag == 0:
                 print("\n Record Not Found!!!")
             file.close()
-
+    elif n == 4:
+        search = input("Enter Student Roll No: ")
+        file = open("Record/student.txt", "r")
+        flag = 0
+        while True:
+            content = file.readline()
+            length = len(content)
+            if length == 0:
+                break
+            arr = content.split("-")
+            if arr[1] == search:
+                print("\n Record Found!!! ")
+                print("Name: ", arr[0])
+                print("Roll No: ", arr[1])
+                print("Class: ", arr[2])
+                print("Fees: ", arr[3])
+                print("Percentage: ", arr[4])
+                flag = 1
+                break
+            if flag == 0:
+                print("\n Record Not Found!!!")
+            file.close()
+    elif n == 5:
+        search = input("Enter Student Name: ")
+        file1 = open("Record/student.txt", "r")
+        file2 = open("Record/temp.txt", "w")
+        flag = 0
+        while True:
+            content = file1.readline()
+            length = len(content)
+            if length == 0:
+                arr = content.split("-")
+                if arr[0] != search:
+                    file2.write(content)
+                if arr[0] == search:
+                    flag = 1
+            file1.close()
+            file2.close()
+            if flag == 1:
+                print("Record Deleted Successfully!!!")
+                os.remove("Record/student.txt")
+                os.rename("Record/temp.txt","Record/student.txt")
+            elif flag == 0:
+                print("Deletion Unsuccessful As Record Not Found!!!")
 
 
 
