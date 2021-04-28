7ea9609194a5980ee6d380e5a7ab0b3ce25ae012
tareninayak9921 <tareninayak9921@gmail.com>
2021-04-28 18:52:48 +0530

    Running Tillm 3rd Function

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
     print("\n 6. Update Student Record")
     print("\n 7. Exit")
 
-    n = int(print(input("Enter Your Choice:  ")))
+    n = int((input("Enter Your Choice:  ")))
     if n == 7:
         break
     elif n == 1:
@@ -33,6 +33,28 @@ while True:
                 break
             print(content.strip())
         file.close()
+    elif n == 3:
+        search = input("Enter Student Name: ")
+        file = open("Record/student.txt", "r")
+        flag = 0
+        while True:
+            content = file.readline()
+            length = len(content)
+            if length == 0:
+                break
+            arr = content.split("-")
+            if arr[0] == search:
+                print("\n Record Found!!! ")
+                print("Name: ", arr[0])
+                print("Roll No: ", arr[1])
+                print("Class: ", arr[2])
+                print("Fees: ", arr[3])
+                print("Percentage: ", arr[4])
+                flag=1
+                break
+            if flag == 0:
+                print("\n Record Not Found!!!")
+            file.close()
 
 
 
